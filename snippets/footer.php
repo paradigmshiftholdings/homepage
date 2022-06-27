a4d07e03c554d5e25b67de4620b84005c8a3be66
kwazeme <97014623+kwazeme@users.noreply.github.com>
2022-06-27 15:10:23 +0200

    contact page

---
+++ snippets/footer.php
@@ -0,0 +1,82 @@
+<footer class="py-2">
+      <div class="container footer-inner">
+        <div class="footer-left">
+          <img src="img/para-footer.png" alt="paradigmshift" class="footer-logo" />
+          <p class="py-1"><h2>Company Values:</h2> 
+            Wherever we operate, Paradigm Shift Holdings will
+            always strive to live by its core values of:
+            <ul>
+              <li><strong>People:</strong> we pledge continuous professional development.</li>
+              <li><strong>Excellence: </strong> we deliver beyond expectations.</li>
+              <li><strong>Customer focus:</strong> we ensure client satisfaction.</li>
+              <li><strong>Innovation:</strong> we create value adding solutions.</li>
+              <li><strong>Integrity:</strong> we foster honest relationships.</li>
+            </ul>
+          </p>
+          <ul class="social-media">
+            <li>
+              <a href="http://facebook.com" aria-label="Facebook"
+                ><i data-feather="facebook"></i
+              ></a>
+            </li>
+            <li>
+              <a href="http://instagram.com" aria-label="Instagram"
+                ><i data-feather="instagram"></i
+              ></a>
+            </li>
+            <li>
+              <a href="http://twitter.com" aria-label="Twitter">
+                <i data-feather="twitter"></i
+              ></a>
+            </li>
+            <li>
+              <a href="http://youtube.com" aria-label="YouTube">
+                <i data-feather="youtube"></i
+              ></a>
+            </li>
+            <li>
+              <a href="http://linkedin.com" aria-label="LinkedIn">
+                <i data-feather="linkedin"></i
+              ></a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-middle">
+          <h1 class="heading-m-thin">Useful links</h1>
+          <ul class="footer-links py-1">
+            <li><span class="pr-075">></span><a href="about.html">About</a></li>
+            <li>
+              <span class="pr-075">></span><a href="subsidiaries.html">Subsidiaries</a>
+            </li>
+            <li>
+              <span class="pr-075">></span><a href="contact.html">Contact</a>
+            </li>
+            <li>
+              <span class="pr-075">></span><a href="projects.html">Projects</a>
+            </li>
+            <li><span class="pr-075">></span><a href="#">Link1</a></li>
+            <li><span class="pr-075">></span><a href="#">Link2</a></li>
+            <li><span class="pr-075">></span><a href="#">Link3</a></li>
+            <li><span class="pr-075">></span><a href="#">Link4</a></li>
+            <li><span class="pr-075">></span><a href="#">Link5</a></li>
+          </ul>
+        </div>
+        <div class="footer-right">
+          <h1 class="heading-m-thin">Contact us</h1>
+          <h2 class="heading-lg pt-05">(017) 123 0910</h2>
+          <ul class="footer-contact">
+            <li>
+              <i data-feather="mail" class="footer-icon"></i
+              ><a href="mailto:#">hello@paradigmshiftholdings.com</a>
+            </li>
+            <li>
+              <i data-feather="globe" class="footer-icon"></i
+              ><a href="https://paradigmshiftholdings.com">www.paradigmshiftholdings.com</a>
+            </li>
+            <li>
+              <i data-feather="map-pin" class="footer-icon"></i>02 Sering Street, Secunda, 2302 (Mpumalanga)
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
